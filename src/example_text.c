c15a7bd511d5b9551c35526f9e5d8b9729cbef20
reese <satchelfrost@gmail.com>
2026-04-05 22:44:29 -0400

    just need to add to API

--- src/example_text.c
+++ src/example_text.c
@@ -11,42 +11,77 @@
 #define RGFWDEF extern
 #include "./external/RGFW.h"
 
-#define BITMAP_WIDTH 180
-#define BITMAP_HEIGHT 170
+#define DEFAULT_BITMAP_WIDTH 180
+#define DEFAULT_BITMAP_HEIGHT 170
 #define FIRST_CHAR 32
 #define CHAR_COUNT 96 
 
-unsigned char bitmap[BITMAP_WIDTH*BITMAP_HEIGHT];
-stbtt_bakedchar cdata[CHAR_COUNT]; // ASCII 32..126 is 95 glyphs
+typedef struct {
+   unsigned short x0, y0, x1, y1;
+   float x_offset, y_offset, x_advance;
+} Glyph;
 
-void draw_text_at_base(const char *text, size_t len, int x_start, int y_start, Color color)
+typedef struct {
+    float height;
+    int bitmap_width;
+    int bitmap_height;
+    uint8_t *bitmap;
+    Glyph glyphs[CHAR_COUNT]; // ASCII 32..126 is 95 glyphs
+} Font;
+
+Font load_font(const char *file_path, int font_height)
 {
-    int adv = 0;
+    bool result = true;
+
+    Font font = {
+        .bitmap_width = DEFAULT_BITMAP_WIDTH,
+        .bitmap_height = DEFAULT_BITMAP_HEIGHT,
+        .bitmap = malloc(DEFAULT_BITMAP_WIDTH*DEFAULT_BITMAP_HEIGHT),
+        .height = font_height,
+    };
+
+    String_Builder sb = {0};
+    if (!read_entire_file(file_path, &sb)) return_defer(false);
+    stbtt_BakeFontBitmap((unsigned char *)sb.items, 0, font.height, font.bitmap,
+                         font.bitmap_width, font.bitmap_height, FIRST_CHAR, CHAR_COUNT, (stbtt_bakedchar*)font.glyphs);
+
+defer:
+    sb_free(sb);
+    if (!result) {
+        free(font.bitmap);
+    }
+    return font;
+}
+
+void unload_font(Font font)
+{
+    free(font.bitmap);
+}
+
+void draw_text_at_base(Font font, const char *text, size_t len, int x_start, int y_start, Color color)
+{
+    int x_adv = 0;
     for (size_t i = 0; i < len; i++) {
         uint8_t ch = text[i];
         if (!(FIRST_CHAR <= ch && ch < (CHAR_COUNT + FIRST_CHAR))) continue;
-        stbtt_bakedchar c = cdata[ch-FIRST_CHAR];
-        int x, y;   // index into bitmap
-        int xp, yp; // where we draw
-        for (y = c.y0, yp = y_start; y <= c.y1; y++, yp++) {
-            for (x = c.x0, xp = x_start; x <= c.x1; x++, xp++) {
-                color.a = ((uint8_t*)bitmap)[y*BITMAP_WIDTH + x];
-                if (color.a) draw_pixel(xp+c.xoff + adv, yp+c.yoff, color);
+        Glyph glyph = font.glyphs[ch-FIRST_CHAR];
+        int x, y;   // bitmap index
+        int xp, yp; // pen position
+        for (y = glyph.y0, yp = y_start; y <= glyph.y1; y++, yp++) {
+            for (x = glyph.x0, xp = x_start; x <= glyph.x1; x++, xp++) {
+                color.a = font.bitmap[y*DEFAULT_BITMAP_WIDTH + x];
+                int coord_x = xp + glyph.x_offset + x_adv;
+                int coord_y = yp + glyph.y_offset;
+                if (color.a) draw_pixel(coord_x, coord_y, color);
             }
         }
-        adv += c.xadvance;
+        x_adv += glyph.x_advance;
     }
 }
 
 int main()
 {
-    String_Builder sb = {0};
-    const char *file = "assets/RobotoMono-Medium.ttf";
-    if (!read_entire_file(file, &sb)) return 1;
-    float pixel_height = 32.0f;
-    stbtt_BakeFontBitmap((unsigned char *)sb.items, 0, pixel_height, bitmap,
-                         BITMAP_WIDTH, BITMAP_HEIGHT, FIRST_CHAR, CHAR_COUNT, cdata);
-
+    Font font = load_font("assets/RobotoMono-Medium.ttf", 32);
     int screen_width = 1400;
     int screen_height = 250;
     init_window(screen_width, screen_height, "example text");
@@ -55,7 +90,7 @@ int main()
     char all_chars[CHAR_COUNT];
     for (int i = 0; i < CHAR_COUNT; i++) all_chars[i] = FIRST_CHAR + i;
     int idx = 0;
-    int width = cdata[0].xadvance; // since this is monospace all xadvance values are the same
+    int width = font.glyphs[0].x_advance; // since this is monospace all xadvance values are the same
 
     while (!window_should_close()) {
         if (RGFW_isKeyPressed(RGFW_right)) idx++;
@@ -64,14 +99,14 @@ int main()
         begin_drawing(WHITE);
             /* write a message on the first line (half height) */
             draw_line(0, screen_height/2, screen_width, screen_height/2, BLACK);
-            draw_text_at_base(msg, strlen(msg), 0, screen_height/2.0, BLUE);
+            draw_text_at_base(font, msg, strlen(msg), 0, screen_height/2.0, BLUE);
             Rectangle r = { .x = idx*width, .y = screen_height/2.0, .width = width, .height = 5, };
             draw_rectangle(r, RED);
 
             /* list all of the characters */
-            draw_line(0, screen_height/2+pixel_height, screen_width, screen_height/2+pixel_height, BLACK);
-            draw_text_at_base(all_chars, CHAR_COUNT, 0, screen_height/2.0+pixel_height, BLUE);
-            r.y += pixel_height;
+            draw_line(0, screen_height/2+font.height, screen_width, screen_height/2+font.height, BLACK);
+            draw_text_at_base(font, all_chars, CHAR_COUNT, 0, screen_height/2.0+font.height, BLUE);
+            r.y += font.height;
             draw_rectangle(r, RED);
 
         end_drawing();
