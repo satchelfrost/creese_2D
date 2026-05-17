9751fe5410a5c526f3bb4add9b0e987bc15dcc9a
Reese Gallagher <satchelfrost@gmail.com>
2026-05-16 22:58:49 -0400

    flames to torches, debug flag, factored out load_level1

--- src/game_main.c
+++ src/game_main.c
@@ -8,6 +8,8 @@
  * Any new c includes do need to update nob.c's game_dep_srcs[] */
 #include "game_example_dependency.c"
 
+#define DEBUG 1
+
 typedef enum {
     ELEMENT_NONE,
     ELEMENT_FIRE,
@@ -32,6 +34,12 @@ typedef struct {
 typedef struct {
     V2i position;
     Image image;
+
+    struct {
+        V2i position;
+        Sprite sprite;
+        Rectangle rect;
+    } flame;
 } Torch;
 
 enum {
@@ -44,6 +52,7 @@ typedef struct {
     Torch torches[TORCH_COUNT];
     int torch_padding;
     int torch_scale;
+    int torch_flame_scale;
 
     Rectangle barrier;
     bool barrier_burnt;
@@ -59,6 +68,9 @@ typedef struct {
     Image image;
     Sprite sprite;
 } Player;
+
+static int window_width = 800;
+static int window_height = 640;
 
 void render_cursor(Cursor cursor)
 {
@@ -88,10 +100,67 @@ V2i render_pos_from_bottom_center(V2f bottom_center, Sprite sprite, float scale)
                bottom_center.y - scale*sprite.sub_image.size.y);
 }
 
+Level1 load_level1(Sprite flame_sprite)
+{
+    Level1 level1 = {0};
+
+    /* resourc allocation which we must free later */
+    Image torch_image  = load_image("assets/torch.png");
+    assert(torch_image.data);
+    Image level1_image = load_image("assets/level1.png");
+    assert(level1_image.data);
+
+    /* initialize torches */
+    level1.torch_padding = 100;
+    level1.torch_scale = 3;
+    level1.torch_flame_scale = 2.0;
+    level1.torches[TORCH_LEFT].image = torch_image;
+    level1.torches[TORCH_LEFT].position.x = level1.torch_padding;
+    level1.torches[TORCH_LEFT].position.y = level1.torch_padding;
+    level1.torches[TORCH_LEFT].flame.position.x = level1.torches[TORCH_LEFT].position.x;
+    level1.torches[TORCH_LEFT].flame.sprite = flame_sprite; // cursor is responsible for freeing this
+    level1.torches[TORCH_LEFT].flame.sprite.scale = level1.torch_flame_scale;
+    level1.torches[TORCH_LEFT].flame.position.y  = level1.torches[TORCH_LEFT].position.y;
+    level1.torches[TORCH_LEFT].flame.position.y -= level1.torches[TORCH_LEFT].flame.sprite.image.height*level1.torch_flame_scale;
+    level1.torches[TORCH_LEFT].flame.rect.x = level1.torches[TORCH_LEFT].flame.position.x;
+    level1.torches[TORCH_LEFT].flame.rect.y = level1.torches[TORCH_LEFT].flame.position.y;
+    level1.torches[TORCH_LEFT].flame.rect.width  = level1.torches[TORCH_LEFT].flame.sprite.sub_image.size.x*level1.torch_flame_scale;
+    level1.torches[TORCH_LEFT].flame.rect.height = level1.torches[TORCH_LEFT].flame.sprite.sub_image.size.y*level1.torch_flame_scale;
+
+    level1.torches[TORCH_RIGHT].image = torch_image;
+    level1.torches[TORCH_RIGHT].position.x = window_width - level1.torch_padding - level1.torches[TORCH_RIGHT].image.width*level1.torch_scale;
+    level1.torches[TORCH_RIGHT].position.y = level1.torch_padding;
+    level1.torches[TORCH_RIGHT].flame.sprite = flame_sprite; // cursor is responsible for freeing this
+    level1.torches[TORCH_RIGHT].flame.sprite.scale = level1.torch_flame_scale;
+    level1.torches[TORCH_RIGHT].flame.position.x = level1.torches[TORCH_RIGHT].position.x;
+    level1.torches[TORCH_RIGHT].flame.position.y  = level1.torches[TORCH_RIGHT].position.y;
+    level1.torches[TORCH_RIGHT].flame.position.y -= level1.torches[TORCH_RIGHT].flame.sprite.image.height*level1.torch_flame_scale;
+    level1.torches[TORCH_RIGHT].flame.rect.x = level1.torches[TORCH_RIGHT].flame.position.x;
+    level1.torches[TORCH_RIGHT].flame.rect.y = level1.torches[TORCH_RIGHT].flame.position.y;
+    level1.torches[TORCH_RIGHT].flame.rect.width  = level1.torches[TORCH_RIGHT].flame.sprite.sub_image.size.x*level1.torch_flame_scale;
+    level1.torches[TORCH_RIGHT].flame.rect.height = level1.torches[TORCH_RIGHT].flame.sprite.sub_image.size.y*level1.torch_flame_scale;
+
+    /* barrier which must be burnt */
+    level1.barrier.x = 100;
+    level1.barrier.y = 400;
+    level1.barrier.width  = 50;
+    level1.barrier.height = 100;
+
+    /* image for the level (aka the map/room)*/
+    level1.image = level1_image;
+    level1.image_scale = 2;
+
+    return level1;
+}
+
+void unload_level1(Level1 level1)
+{
+    unload_image(level1.torches[TORCH_LEFT].image); // left and right images were shared
+    unload_image(level1.image);
+}
+
 int main()
 {
-    int window_width = 800;
-    int window_height = 640;
     // disable_mouse_cursor();
     init_audio_device();
     init_window(window_width, window_height, "Creese 2D First Ever Jam!");
@@ -107,24 +176,7 @@ int main()
     String_Builder sb = {0};
 
     /* initialize level */
-    Level1 level1 = {0};
-    level1.torch_padding = 100;
-    level1.torch_scale = 3;
-    level1.torches[TORCH_LEFT].image = load_image("assets/torch.png");
-    level1.torches[TORCH_LEFT].position.x = level1.torch_padding;
-    level1.torches[TORCH_LEFT].position.y = level1.torch_padding;
-    level1.torches[TORCH_RIGHT].image = level1.torches[TORCH_LEFT].image; // be careful not to double free
-    level1.torches[TORCH_RIGHT].position.x = window_width - level1.torch_padding - level1.torches[TORCH_RIGHT].image.width*level1.torch_scale;
-    level1.torches[TORCH_RIGHT].position.y = level1.torch_padding;
-
-    level1.barrier.x = 100;
-    level1.barrier.y = 400;
-    level1.barrier.width  = 50;
-    level1.barrier.height = 100;
-
-    level1.image = load_image("assets/level1.png");
-    level1.image_scale = 2;
-    if (!level1.image.data) return 1;
+    Level1 level1 = load_level1(cursor.flame.sprite);
 
     Player player = {0};
     player.image = load_image("assets/player.png");;
@@ -145,13 +197,7 @@ int main()
 
         /* physics/state */
         for (int i = 0; i < TORCH_COUNT; i++) {
-            Rectangle collision_rect = {
-                .x = level1.torches[i].position.x,
-                .y = level1.torches[i].position.y,
-                .width  = level1.torch_scale*level1.torches[i].image.width,
-                .height = level1.torch_scale*level1.torches[i].image.height,
-            };
-            if (cursor.pressed && rectangle_contains(collision_rect, cursor.position.x, cursor.position.y)) {
+            if (cursor.pressed && rectangle_contains(level1.torches[i].flame.rect, cursor.position.x, cursor.position.y)) {
                 cursor.element = ELEMENT_FIRE;
                 play_sound(cursor.flame.sound);
             }
@@ -166,6 +212,8 @@ int main()
 
         /* update animations */
         update_animation(&cursor.flame.sprite, 7);
+        update_animation(&level1.torches[TORCH_LEFT].flame.sprite, 7);
+        update_animation(&level1.torches[TORCH_RIGHT].flame.sprite, 7);
         update_animation(&player.sprite, 2);
 
         /* drawing */
@@ -177,18 +225,26 @@ int main()
             player.render_position = render_pos_from_bottom_center(player.position, player.sprite, player.scale);
             draw_sprite(player.sprite, player.render_position.x, player.render_position.y);
 
-            /* cursor */
-            render_cursor(cursor);
-            for (int i = 0; i < TORCH_COUNT; i++)
+            /* torches */
+            for (int i = 0; i < TORCH_COUNT; i++) {
                 draw_image_scaled(level1.torches[i].image, level1.torches[i].position.x, level1.torches[i].position.y, 3, 3);
+                draw_sprite(level1.torches[i].flame.sprite, level1.torches[i].flame.position.x, level1.torches[i].flame.position.y);
+
+                if (DEBUG)
+                    draw_rectangle_lines(level1.torches[i].flame.rect, YELLOW);
+            }
             draw_rectangle(level1.barrier, (level1.barrier_burnt) ? BLACK : BLUE);
 
-            /* debug */
-            Rectangle fps_rect = {.width = 125, .height = font.height+5};
-            draw_rectangle(fps_rect, WHITE);
-            sb.count = 0; // reuse string builder memory
-            sb_appendf(&sb, "FPS:%d", get_avg_fps());
-            draw_text_at_base(font, sb.items, sb.count, 10, font.height, BLACK);
+            /* cursor */
+            render_cursor(cursor);
+
+            if (DEBUG) {
+                Rectangle fps_rect = {.width = 125, .height = font.height+5};
+                draw_rectangle(fps_rect, WHITE);
+                sb.count = 0; // reuse string builder memory
+                sb_appendf(&sb, "FPS:%d", get_avg_fps());
+                draw_text_at_base(font, sb.items, sb.count, 10, font.height, BLACK);
+            }
         end_drawing();
     }
 
